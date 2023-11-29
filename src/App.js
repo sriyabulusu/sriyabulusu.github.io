e483e53e0e0713e138a991a476a33f6a66831ea5
sriyabulusu <bulusu@cs.washington.edu>
2023-11-29 02:08:50 -0800

    changing colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello, I'm Sriya</p>
+    <div className='App'>
+      <div className='header'>
+        <p className='text'>Hello, I'm Sriya</p>
+        <p className='text'>I am a sophomore at the University of Washington.</p>
+      </div>
     </div>
   );
 }
